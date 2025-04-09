27d0cd6277c916cbfe852090e8c2f39b90284f33
shubham karale <skarale63@gmail.com>
2025-04-09 22:39:47 +0530

    Create repo

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import find_packages, setup
 setup(
     name = 'Generative AI Project',
     version= '0.0.0',
-    author= 'Bappy',
-    author_email= 'boktiar@ineuron.ai',
+    author= 'Shubham',
+    author_email= 'skarale63@gmail.com',
     packages= find_packages(),
     install_requires = []
 
