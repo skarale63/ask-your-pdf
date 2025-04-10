851af7a671919e2eda517bb892721c0510159a95
shubham karale <skarale63@gmail.com>
2025-04-10 14:24:57 +0530

    Update files with ollama llm

--- src/helper.py
+++ src/helper.py
@@ -1,17 +1,22 @@
 import os
 from PyPDF2 import PdfReader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.embeddings import GooglePalmEmbeddings
-from langchain.llms import GooglePalm
-from langchain.vectorstores import FAISS
+from langchain.embeddings import OllamaEmbeddings
+from langchain.llms import Ollama
+from langchain_community.vectorstores.faiss import FAISS
 from langchain.chains import ConversationalRetrievalChain
 from langchain.memory import ConversationBufferMemory
 from dotenv import load_dotenv
 
 
 load_dotenv()
-GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")  
+GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY") 
+LANGCHAIN_API_KEY = os.getenv("LANGCHAIN_API_KEY")  
+LANGCHAIN_PROJECT = os.getenv("LANGCHAIN_PROJECT")  
+
 os.environ['GOOGLE_API_KEY'] =  GOOGLE_API_KEY
+os.environ['LANGCHAIN_API_KEY'] =  LANGCHAIN_API_KEY
+os.environ['LANGCHAIN_PROJECT'] =  LANGCHAIN_PROJECT
 
 
 def get_pdf_text(pdf_docs):
@@ -32,14 +37,14 @@ def get_text_chunks(text):
 
 
 def get_vector_store(text_chunks):
-    embeddings = GooglePalmEmbeddings()
+    embeddings = OllamaEmbeddings()
     vector_store = FAISS.from_texts(text_chunks, embedding=embeddings)
     return vector_store
 
 
 
 def get_conversational_chain(vector_store):
-    llm=GooglePalm()
+    llm=Ollama()
     memory = ConversationBufferMemory(memory_key = "chat_history", return_messages=True)
     conversation_chain = ConversationalRetrievalChain.from_llm(llm=llm, retriever=vector_store.as_retriever(), memory=memory)
     return conversation_chain
